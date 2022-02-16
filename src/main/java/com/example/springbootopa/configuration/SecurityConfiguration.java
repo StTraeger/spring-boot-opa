cd4ebd48f6f11292ff92952d872b76929f1e3eef
Stefan Traeger <straeger11@web.de>
2022-02-16 20:44:10 +0100

    Adapt SecurityConfiguration to only secure the /secured api endpoint.

--- src/main/java/com/example/springbootopa/configuration/SecurityConfiguration.java
+++ src/main/java/com/example/springbootopa/configuration/SecurityConfiguration.java
@@ -24,7 +24,7 @@ public class SecurityConfiguration extends WebSecurityConfigurerAdapter {
     protected void configure(final HttpSecurity http) throws Exception {
         http
                 .authorizeRequests()
-                .anyRequest().authenticated()
+                .antMatchers("/demo/secured").authenticated()
                 .accessDecisionManager(accessDecisionManager());
     }
 }
